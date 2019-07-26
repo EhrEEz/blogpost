f86629742125316000f3a4efe3d2094c87f16bc4
Ares Shrestha <aresshrestha@outlook.com>
2019-07-26 09:01:09 +0545

    Added Post Edit and Post Edit Authentication

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.humanize",
     "bootstrap4",
     "post",
+    "moment",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- blog/urls.py
+++ blog/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path, include
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("index/", include("post.urls")),
-    path("", include("post.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("index/", include("post.urls")),
+        path("", include("post.urls")),
+        path("accounts/", include("django.contrib.auth.urls")),
+    ]
+    + staticfiles_urlpatterns()
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
