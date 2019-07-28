7b22cbeb3568bb49e3be6c565c17eae9e323375a
Ares Shrestha <aresshrestha@outlook.com>
2019-07-28 19:06:24 +0545

    Added Navigation Bar Javascripting

---
+++ static/js/nav.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var scroll_start = 0;
+  var startchange = $('.container-fluid');
+  var offset = startchange.offset();
+    if (startchange.length){
+  $(document).scroll(function(){
+    scroll_start = $(this).scrollTop();
+    if(scroll_start >= offset.top) {
+      $('#grad1').css('background', 'rgba(255,255,255,0.9)');
+      $('#nav_rt').css('color', '#000000');
+      $('.nav-link, h6').css('color', '#000000');
+      $('.navbar-brand').css('color', '#000000');
+      $('.nav-link.active').css('color', '#EB7C79');
+
+    } else {
+      $('#grad1').css('background', 'rgba(0,0,0,0)');
+      $('#nav_rt').css('color', 'rgba(255,255,255,0.7)');
+      $('.nav-link, h6').css('color', 'rgba(255,255,255,0.7)');
+      $('.navbar-brand').css('color', 'rgba(255,255,255,0.7)');
+      $('.nav-link.active').css('color', '#EB7C79');
+    }
+  });
+
+    }
+});
